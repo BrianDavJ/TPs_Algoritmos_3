23d20583248cbf92c015e9612d954e1f3919430e
Brian <brian.d.jaime97@gmail.com>
2023-04-15 15:37:56 -0300

    a

--- tp-ej2.cpp
+++ tp-ej2.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 int C;
-vector<vector<int>> mem;
+vector<vector<vector<int>>> mem;
 
-int mod_bin_exp(int x, int y, int m) {
+long long int mod_bin_expM(int x, int y, int m) {
     if (y == 0) return 1;
 
-    int partialres = mod_bin_exp(x, y/2, m);
+    long long int partialres = mod_bin_expM(x%m, (y/2)%m, m);
 
 
     if (y % 2 == 0){
@@ -22,38 +22,38 @@ int mod_bin_exp(int x, int y, int m) {
 }
 
 bool operaciones(vector<int> v, int r, int m, int index, int restoParcial,int op){
-    
-    if (index == v.size()) return (restoParcial % m == r);
-    
-    int temp;
     bool res = false;
 
-    if(mem[index][restoParcial] == -1){
-
+    if(mem[index][restoParcial][op] == -1){
+        int temp;
         switch (op)
         {
-        case 0:
-            temp = (restoParcial + v[index]) % m;
-            break;
-        case 1:
-            temp = (restoParcial - v[index]) % m;
-            break;
-        case 2:
-            temp = (restoParcial * v[index]) % m;
-            break;
-        case 3:
-            temp = mod_bin_exp(restoParcial,v[index],m);
-            break;
+            case 0:
+                temp = (restoParcial + v[index]) % m;
+                break;
+            case 1:
+                cerr << "llego" << endl;
+                temp = (restoParcial - v[index]) % m;
+                break;
+            case 2:
+                temp = (restoParcial * v[index]) % m;
+                break;
+            case 3:
+                temp = mod_bin_expM(restoParcial,v[index],m);
+                break;
         }
-
-        for(int i = 0; i < 4; i ++){
-            res = res || operaciones(v, r, m, index+1, temp, i);
+        if (index==v.size()-1){
+            mem[index-1][temp][op]=temp % m == r;
+            return temp % m == r;
+        }else{
+            for(int i = 0; i < 4; i ++){
+                res = res || operaciones(v, r, m, index+1, temp, i);
+            }
         }
-
-        mem[index][restoParcial] = res;
+        mem[index][restoParcial][op] = res;
         return res;
     }
-    else return mem[index][restoParcial];
+    else return mem[index][restoParcial][op];
 
 }
 
@@ -67,7 +67,7 @@ int main(){
         cin >> r;
         cin >> m;
 
-        mem = vector<vector<int>>(size,vector<int>(m,-1));
+        mem = vector<vector<vector<int>>>(size,vector<vector<int>>(m,vector<int>(4,-1)));
 
         vector<int> v(size);
 

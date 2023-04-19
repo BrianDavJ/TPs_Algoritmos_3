6943382f1ab2a115717987422a8698931dd3c3de
gaspush <pizzogaspar@gmail.com>
2023-04-19 17:41:33 -0300

    &

--- tp-ej2.cpp
+++ tp-ej2.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <map>
-#include <tuple>
 #include <math.h>
 #include <limits>
 using namespace std;
 
 long long C;
 vector<vector<int>> mem;
-map<tuple<int,int>,bool> memjorada;
 
 long long int mod_bin_expM(int x, int y, int m) {//Calcula mediante un algoritmo D&C, el resto de la potencia, sin exceder el tamanio de memoria del dato
 
@@ -24,12 +21,12 @@ long long int mod_bin_expM(int x, int y, int m) {//Calcula mediante un algoritmo
     }
 }
 
-bool operaciones(vector<long long > v, long long r, long long m, int index, long long int restoParcial,int op){
+bool operaciones(vector<long long >& v, long long r, long long m, int index, long long int restoParcial,int op){
     bool res = false;
+
 
     if (index == v.size()) return restoParcial % m == r;
 
-    
 
         long long int temp;
         switch (op)//Realizamos la operacion indicada por parametro, asegurandonos de no conservar un resto negativo
@@ -48,28 +45,26 @@ bool operaciones(vector<long long > v, long long r, long long m, int index, long
                 break;
             case 3:
                 if (restoParcial == 0 && v[index] == 0){// 0 ^ 0 INDETERMINADO, no podemos realizar esta operacion
-                    //mem[index][restoParcial] = false;
-                    tuple<int,int> tup = make_tuple(index,restoParcial);
-                    memjorada[tup] = false;
+                    mem[index][restoParcial] = false;
                     return false;
                 }
                 temp = mod_bin_expM(restoParcial,v[index],m);
                 break;
         }
 
-    tuple<int,int> tup = make_tuple(index,temp);
-    if(!memjorada.count(tup)){//Si en la posicion correspondiente de la matriz (diccionario ahora )tenemos -1 (no esta definido), realizamos el calculo recursivo
+
+    //Si en la posicion de la matriz correspondiente a este indice y este resto tenemos -1, realizamos el calculo recursivo.
+    if(mem[index][temp] == -1){
 
         for(int i = 0; i < 4; i ++){
             res = res || operaciones(v, r, m, index+1, temp, i);
         }
-        //mem[index][temp] = res;
-        memjorada[tup] = res;
+        mem[index][temp] = res;
         return res;
     }
 
     //Si la matriz contenia un valor valido, este caso ya fue calculado y lo retorno.
-    else return memjorada[tup];
+    else return mem[index][temp];
 
 }
 
@@ -87,7 +82,7 @@ int main(){//Ejercicio 2
         //La memoria tiene dimensiones |v| * m
         //En la posicion [n][r] de la matriz, se encuentra un "booleano" que nos indica si es posible llegar a la solucion deseada
         //desde la posicion n del vector, teniendo un resto parcial r. De haber un -1, no ha sido calculado aun.
-        //mem = vector<vector<int>>(size,vector<int>(m,-1));
+        mem = vector<vector<int>>(size,vector<int>(m,-1));
 
         vector<long long> v(size);
 

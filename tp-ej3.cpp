0d1e678f8d0fda5405b1cc2997ebc567d8251b55
Brian D. Jaime <73399486+BrianDavJ@users.noreply.github.com>
2023-04-19 15:44:55 -0300

    bucketsort y agarrar los que no se solapen
    Falta escribir el main bien, no esta probado ni el bucket ni tomar los primeros que no se solapen.

--- tp-ej3.cpp
+++ tp-ej3.cpp
@@ -1,11 +1,59 @@
+#include <algorithm>
 #include <iostream>
-#include <vector>
-#include <math.h>
-#include <limits>
+#include <vector> 
+#include <tuple>
 using namespace std;
 
-int main(){
+void bucktsort(vector <tuple<int,int>> Actividades){
+    int longitud=Actividades.size();
+    int rango=(2*longitud-1)/longitud;
+    vector<vector<tuple<int,int>>> balde;
+     for (int i = 0; i < 2*longitud; i++) {
+    balde.push_back(vector<tuple<int,int>>(0));
+  }
+  for (int i = 0; i < longitud; i++) {
+    int diff = (get<1>(Actividades[i]) - 1) / rango;
+  
+    if (diff == 0 && get<1>(Actividades[i]) != 1) {
+      balde[((get<1>(Actividades[i]) - 1) / rango) - 1].push_back(Actividades[i]);
+    }
+    else {
+      balde[(get<1>(Actividades[i]) - 1) / rango].push_back(Actividades[i]);
+    }
+  }
+   for (int i = 0; i < balde.size(); i++) {
+    if (!balde[i].empty()) {
+      sort(balde[i].begin(), balde[i].end());
+    }
+  }
+    int k = 0;
+  for (vector<tuple<int,int>>& lst : balde) {
+    if (!lst.empty()) {
+      for (tuple<int,int> i : lst) {
+        Actividades[k] = i;
+        k++;
+      }
+    }
+  }
+}
 
-    
+
+vector<tuple<int,int>> agarrarPrimerosSinSolapar(vector<tuple<int,int>> Arr){
+    vector<tuple<int,int>> res(0);
+    res.push_back(Arr[0]);
+    for(int i=1;i<Arr.size();i++){
+        if(get<1>(Arr[i-1]) <=get<0>(Arr[i])){
+            res.push_back(Arr[i]);
+        }
+    }
+    return res;
+}
+
+
+int main(){
+    vector<tuple<int,int>> grandeGoloSO(vector <tuple<int,int>> Actividades){
+        bucktsort(Actividades);
+        return agarrarPrimerosSinSolapar(Actividades);
+    }
     return 0;
 }

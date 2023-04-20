96069e7aa7f764f3e8c12ec8fbfec6b6f1caf717
gaspush <pizzogaspar@gmail.com>
2023-04-20 15:46:50 -0300

    LITOOOOOOOO

--- tp-ej3.cpp
+++ tp-ej3.cpp
@@ -5,37 +5,31 @@
 using namespace std;
 
 int C;
-vector<tuple<int,int>> Actividades;
+vector<tuple<int,int,int>> Actividades;
 
-void bucktsort(vector<tuple<int,int>> Actividades){
+void bucktsort(vector<tuple<int,int,int>>& Actividades){
   int longitud=Actividades.size();
-  int rango=(2*longitud-1)/longitud;
-  vector<vector<tuple<int,int>>> balde;
+  vector<vector<tuple<int,int,int>>> balde;
 
   for (int i = 0; i < 2*longitud; i++) {
-    balde.push_back(vector<tuple<int,int>>(0));
+    balde.push_back(vector<tuple<int,int,int>>(0));
   }
 
   for (int i = 0; i < longitud; i++) {
-    int diff = (get<1>(Actividades[i]) - 1) / rango;
+    int diff = (get<1>(Actividades[i]) - 1);
   
     if (diff == 0 && get<1>(Actividades[i]) != 1) {
-      balde[((get<1>(Actividades[i]) - 1) / rango) - 1].push_back(Actividades[i]);
+      balde[((get<1>(Actividades[i]) - 1)) - 1].push_back(Actividades[i]);
     }
     else {
-      balde[(get<1>(Actividades[i]) - 1) / rango].push_back(Actividades[i]);
+      balde[(get<1>(Actividades[i]) - 1)].push_back(Actividades[i]);
     }
   }
 
-  for (int i = 0; i < balde.size(); i++) {
-    if (!balde[i].empty()) {
-      sort(balde[i].begin(), balde[i].end());
-    }
-  }
     int k = 0;
-  for (vector<tuple<int,int>>& lst : balde) {
+  for (vector<tuple<int,int,int>> lst : balde) {
     if (!lst.empty()) {
-      for (tuple<int,int> i : lst) {
+      for (tuple<int,int,int> i : lst) {
         Actividades[k] = i;
         k++;
       }
@@ -43,8 +37,8 @@ void bucktsort(vector<tuple<int,int>> Actividades){
   }
 }
 
-vector<tuple<int,int>> agarrarPrimerosSinSolapar(vector<tuple<int,int>> Arr){
-    vector<tuple<int,int>> res(0);
+vector<tuple<int,int,int>> agarrarPrimerosSinSolapar(vector<tuple<int,int,int>> Arr){
+    vector<tuple<int,int,int>> res(0);
     res.push_back(Arr[0]);
     for(int i=1;i<Arr.size();i++){
         if(get<1>(res[res.size()-1]) <= get<0>(Arr[i])){
@@ -63,12 +57,18 @@ int main(){
   for (int i = 0; i < C; i++){
     cin >> s_i;
     cin >> t_i;
-    tuple<int,int> tup = make_tuple(s_i, t_i);
+    tuple<int,int,int> tup = make_tuple(s_i, t_i,i);
     Actividades.push_back(tup);
   }
 
   bucktsort(Actividades);
-  agarrarPrimerosSinSolapar(Actividades);
+  vector<tuple<int,int,int>> actRes = agarrarPrimerosSinSolapar(Actividades);
+
+  cout << actRes.size() << endl;
+
+  for(int i = 0; i < actRes.size(); i++){
+    cout << get<2>(actRes[i])+1 << endl;
+  }
 
   return 0;
 }

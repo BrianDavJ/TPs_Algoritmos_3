a25272576c52b2eb5c59c4292480c250cab0a86c
Brian D. Jaime <73399486+BrianDavJ@users.noreply.github.com>
2023-05-23 17:47:29 -0300

    AAAAAAAAAAAAAAAAAA

--- tp-ej2.cpp
+++ tp-ej2.cpp
@@ -1,98 +1,114 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
-#include <limits>
+#include <tuple>
+#include <iomanip>
+#include <map>
+// #include <math.h> lo usé para testear el setprecision
+
 using namespace std;
+typedef long long ll;
+int tope =100000;
 
-long long C;
-vector<vector<int>> mem;
-
-long long int mod_bin_expM(int x, int y, int m) {//Calcula mediante un algoritmo D&C, el resto de la potencia, sin exceder el tamanio de memoria del dato
-
-    if (y == 0) return 1;//Sabemos que x != 0 por la guarda en nuestra funcion principal
-
-    long long int partialRes = mod_bin_expM(x%m, (y/2)%m, m);
-
-    if (y % 2 == 0){
-        return (partialRes * partialRes) % m;
-    }
-    else{
-        return (partialRes * partialRes * x) % m;
+int NO_LO_VI = 0, EMPECE_A_VER = 1,TERMINE_DE_VER = 2, n, m;
+vector<int> componentesConexas;// Aqui alamcenaremos los tamanios de las distintas CC una vez "eliminados" los puentes
+vector<vector<int>> adyacencias;
+map<pair<int,int>,bool> puentes;// Mapa de aristas a bool. Si value de una arista es true, esta es puente en mi arbol DFS
+vector<int> estado;
+vector<int> tin; // ENTRO AL ÁRBOL EN QUÉ ITERACION "time in node" 
+vector<int> low; // PUEDO VISITAR UN NODO ANTERIOR DEL ÁRBOL? SI EL VALOR DE CUANDO ENTRÓ N NO ES EL MISMO QUE EL VALOR QUE PUEDO VISITAR ES PORQUE VISITA UNO ANTERIOR
+int compConexa=0;
+int it=0;
+            
+void marcarPuentes(int v, int p){ //re corre el grafo como dfs marcando las aristas puente.
+    estado[v] = EMPECE_A_VER;
+    it++;
+    tin[v] = low[v] = it;  // tin = iteración en la que entra al árbol ése nodo. Low = nodo más cercano en "tiempo" a V que se puede alcanzar con ése nodo.
+    for(int u: adyacencias[v]){  // O(m)
+        if(u == p) continue;
+        if (estado[u] == NO_LO_VI){
+            marcarPuentes(u, v); //sigo bajando marcando en que iteración entran por primera vez
+            low[v] = min(low[u], low[v]);//
+            
+            if(low[u] > low[v]){ // El nodo hijo "u" sólo alcanza nodos posteriores, no hay backedge que cubra a V entonces es puente.
+                puentes[{u, v}] = puentes[{v, u}] = true;
+            }
+        }
+        else{
+            low[v] = min(low[v], tin[u]); // actualizo el nodo más cercano que se puede alcanzar comparando con la backedge
+            puentes[{u, v}] = puentes[{v, u}] = false;
+        }
     }
 }
-
-
-bool operaciones(vector<long long >& v, long long r, long long m, int index, long long int restoParcial,int op){
-    bool res = false;
-
-    if (index == v.size()) return restoParcial % m == r;
-
-        long long int temp;
-        switch (op)//Realizamos la operacion indicada por parametro, asegurandonos de no conservar un resto negativo
-        {
-            case 0:
-                temp = (restoParcial + (v[index]%m)) % m;
-                if (temp < 0) temp += m;
-                break;
-            case 1:
-                temp = (restoParcial - (v[index]%m)) % m;
-                if (temp < 0) temp += m;
-                break;
-            case 2:
-                temp = (restoParcial * (v[index]%m)) % m;
-                if (temp < 0) temp += m;
-                break;
-            case 3:
-                if (restoParcial == 0 && v[index] == 0){// 0 ^ 0 INDETERMINADO, no podemos realizar esta operacion
-                    mem[index][restoParcial] = false;
-                    return false;
-                }
-                temp = mod_bin_expM(restoParcial,v[index],m);
-                break;
+int DFS2(int v, int tam_cc){
+    int temp_tam=tam_cc;
+    estado[v]= EMPECE_A_VER;
+    for(int u: adyacencias[v]){
+        if(estado[u]==NO_LO_VI && !puentes[{u, v}]){//!puentes[{u,v}]){//Ignora los nodos conectados por aristas puente
+            temp_tam++;
+            temp_tam=DFS2(u,temp_tam);
         }
-
-    //Si en la posicion de la matriz correspondiente a este indice y este resto tenemos -1, realizamos el calculo recursivo.
-    if(mem[index][temp] == -1){
-
-        for(int i = 0; i < 4; i ++){
-            res = res || operaciones(v, r, m, index+1, temp, i);
-        }
-        mem[index][temp] = res;
-        return res;
     }
-
-    //Si la matriz contenia un valor valido, este caso ya fue calculado y lo retorno.
-    else return mem[index][temp];
+    estado[v]= TERMINE_DE_VER;
+    return temp_tam;
 }
 
-
-int main(){//Ejercicio 2
-    cin >> C;
-
-    //Inicializo los argumentos para cada uno de los C casos de test.
-    for (int i = 0; i < C; i++){
-        long long size, r, m;
-        cin >> size;
-        cin >> r;
-        cin >> m;
-
-        //La memoria tiene dimensiones |v| * m
-        //En la posicion [n][r] de la matriz, se encuentra un "booleano" que nos indica si es posible llegar a la solucion deseada
-        //desde la posicion n del vector, teniendo un resto parcial r. De haber un -1, no ha sido calculado aun.
-        mem = vector<vector<int>>(size,vector<int>(m,-1));
-
-        vector<long long> v(size);
-
-        for (int j = 0; j < size; j++){
-            cin >> v[j];
+void armarComponentes(){
+    for(int i = 1; i <= n; i ++){
+        if(estado[i] == NO_LO_VI){
+           int tamCC = DFS2(i,1);
+           
+           componentesConexas.push_back(tamCC);
+           /*
+           for (int j=0; j<componentesConexas.size();j++){
+            cout << " " << componentesConexas[j] << " " << endl;
+               
+           }*/
+          
         }
-
-
-        //El llamado inicial a la funcion recursiva se ejecuta desde la posicion 0 del vector, teniendo un resto parcial de 0 y realizando una suma (operacion 0).
-        bool res = operaciones(v,r,m,0,0,0);
-
-        if (res) cout << "Si" << endl;
-        else     cout << "No" << endl;
     }
+}
+ll combinatorio(int n){//Solo nos importa el combinatorio (n 2)
+    
+    return ((n-1)*n / (2));
+}
+int main(){
+    cin >> n >> m;
+    adyacencias = vector<vector<int>>(n+1);
+    //componentesConexas = vector<int>(n+1);
+    estado = vector<int>(tope,NO_LO_VI);
+    tin=vector<int>(tope);
+    low=vector<int>(tope);
+    
+    //ll eleccionesTotales =(n*(n-1)/2);
+    
+    while (m--){
+        int u, v;
+        cin >> u >> v;
+
+        adyacencias[u].push_back(v);
+        adyacencias[v].push_back(u);
+    }
+    for(int i = 1; i <= n; i ++){
+        if (estado[i] == NO_LO_VI){
+            marcarPuentes (i,0);
+        }
+    }
+    estado = vector<int>(n+1,NO_LO_VI);
+    armarComponentes();
+
+    double ganar = 0;
+    for (int i = 0; i < componentesConexas.size(); i++){
+        ganar += combinatorio(componentesConexas[i]);
+    }
+    //cout << ganar << endl;
+    ganar=ganar/(1.0*n/2.0);
+    //cout << ganar << endl;
+    ganar=ganar/(1.0*(n-1.0));
+    //cout << ganar << endl;
+    
+    long double probPerder = (1.0 - ganar);
+    cout << fixed << setprecision(5) << probPerder << endl;
     return 0;
 }
+
+
